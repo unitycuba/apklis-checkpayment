c876815041ff37ef9252d9689ed4720dd5176b63
Tony <70nybl4nc0@gmail.com>
2020-12-16 18:54:00 -0500

    Making things happens :D

--- Runtime/PaymentChecker.cs
+++ Runtime/PaymentChecker.cs
@@ -6,21 +6,28 @@ namespace Apklis.Payment
     {
         public static PaymentStatus IsPurchased(string packageId)
         {
-            var androidClass = new AndroidJavaClass("cu.fluttercuba.apklis_payment_checker.Verify");
+            var player = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            var activity = player.GetStatic<AndroidJavaObject>("currentActivity");
+            
+            var verifyClass = new AndroidJavaClass("cu.fluttercuba.apklis_payment_checker.Verify");
+            var companionObject = verifyClass.GetStatic<AndroidJavaObject>("Companion");
 
-            var result =  androidClass.Call<Response> ("isPurchased", packageId);
+            // TODO: Use the correct methods to get components for the Pair object
+            var result = companionObject.Call<AndroidJavaObject>("isPurchased", activity, packageId).Call<string>("toString"); 
 
-            return new PaymentStatus() {
-                paid = (bool) result.first,
-                username = (string) result.second
+            // removing the parentesis of: (A,B)
+            var resultArray = result.Substring(1, result.Length - 2).Split(',');
+
+            var paid = bool.Parse(resultArray[0]);
+            var username = resultArray[1];
+            
+            return new PaymentStatus()
+            {
+                paid = paid,
+                username = username
             };
         }
 
-        private struct Response
-        {
-            public object first;
-            public object second;
-        }
     }
-    
+
 }
